479529bb7402362ad9ba97fdcfe6c2662aed559f
Gianni Chiappetta <gianni@runlevel6.org>
2010-10-22 16:40:37 -0400

    Initial commit.

---
+++ example/example.js
@@ -0,0 +1,7 @@
+var Colour = require( '../lib/coloured' )
+
+console.log( "So I heard you like " + "red".red() + "?" )
+console.log( "Perhaps you like it " + "on green".red_on_green() + "?" )
+console.log( "And maybe some " + "bold".bold() + "?" )
+console.log( "All together".red_on_green().bold() + "?" )
+
